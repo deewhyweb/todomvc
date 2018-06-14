058f2e052fdfc5daa0197295f452c5270dfa935e
raffaelespazzoli <raffaele.spazzoli@gmail.com>
2018-06-14 15:26:18 +0200

    f

--- examples/angularjs/integration-tests/protractor-conf.js
+++ examples/angularjs/integration-tests/protractor-conf.js
@@ -1,19 +1,28 @@
 exports.config = {
   framework: 'cucumber',
   rootElement: 'body', // location of ng-app directive
-  seleniumAddress: 'http://zalenium.todomvc-stage.svc:4444/wd/hub',
-  capabilities: {
-    'browserName': 'chrome'
-  },
+  seleniumAddress: 'http://zalenium-todomvc-stage.apps.raffa1.casl-contrib.osp.rht-labs.com/wd/hub',
+  multiCapabilities: [{
+	  'browserName': 'firefox'
+	}, {
+	  'browserName': 'chrome'
+	}],
   specs: ['features/*.feature'],
-
+  coloredLogs: false,
   // See cucumberOpts in https://github.com/angular/protractor/blob/master/docs/referenceConf.js
   // A list of tags to run can be specified here e.g.
   // tags: '@smoke'
   // tags: ['@smoke', '@otherTag, @thirdTag']
   // The Cucumber require path can be set with the 'require' property.
   cucumberOpts: {
-    format: 'pretty'
-  }
+    format: 'json:./tmp/results.json'
+  },
+  plugins: [{
+      package: 'protractor-multiple-cucumber-html-reporter-plugin',
+      options:{
+          // read the options part
+    	  automaticallyGenerateReport: true
+      }
+  }]
 };
 
